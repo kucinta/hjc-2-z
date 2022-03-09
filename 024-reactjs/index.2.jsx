248ed2a9607ed816657a88dfe66d22ae04ce6595
kucinta.com <szeye@hotmail.com>
2022-03-10 00:58:53 +0800

    cleanup

--- 024-reactjs/index.2.jsx
+++
@@ -1,7 +0,0 @@
-/*jshint esversion: 6 */
-var HelloMessage = React.createClass({
-    render: function render() {
-        return React.createElement('div', null, 'Hello ', this.props.name);
-    }
-});
-ReactDOM.render(React.createElement(HelloMessage, { name: 'John' }), document.getElementById('app'));
